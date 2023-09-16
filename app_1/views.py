92a2a6db882dfadc6ccb2adaba6b4b5bcd37c1ac
Rian <rlina1785@gmail.com>
2023-09-15 22:29:38 -0300

    novo paragrafo

--- app_1/views.py
+++ app_1/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def saudacao(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    return HttpResponse("<h1>Hello, World!</h1> <p>mudei</p>")
 
