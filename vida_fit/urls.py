7257017ea890fa33e71b16af09f7ab06c18dd57f
Rian <rlina1785@gmail.com>
2024-10-28 14:00:14 -0300

    design da home criada com sucesso, todos as funcionalidades da HOME estão prontas para uso!

--- vida_fit/urls.py
+++ vida_fit/urls.py
@@ -3,4 +3,5 @@ from vida_fit import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('calculator/', views.calculator, name='calculator'),
 ]
--- vida_fit/views.py
+++ vida_fit/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'vida_fit/pages/home.html')
+
+def calculator(request):
+    return render(request, 'vida_fit/pages/calculator.html')
